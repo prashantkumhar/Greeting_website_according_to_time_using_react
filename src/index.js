edfd5d4abff6371afb0cf91eaf441f59b443c4b2
prashantkumhar <106858425+prashantkumhar@users.noreply.github.com>
2022-11-14 20:20:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./styles.css";
+
+const cssStyle = {};
+let greeting = "";
+const d = new Date();
+const x = d.getHours();
+if (x >= 1 && x < 12) {
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+} else if (x >= 12 && x < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "orange";
+} else {
+  greeting = "Good night";
+  cssStyle.color = "lite-red";
+}
+ReactDom.render(
+  <>
+    <div>
+      <h1>
+        Hello sir, <span style={cssStyle}> {greeting}</span>{" "}
+      </h1>
+    </div>
+  </>,
+  document.getElementById("root")
+);
